a55d960f3aab331ea18e4d5344bba9d7b7f8ad85
Anton Voronko <avor@corp.isddesign.com>
2020-11-09 10:28:31 +0200

    connection to mongodb

--- app.py
+++ app.py
@@ -3,25 +3,55 @@ from flask import Flask, jsonify, request
 from preprocessing import json_to_dataframe, dataset_preparation
 from prediction import prediction
 import json
+from models import MongoAPI
+from pprint import pprint
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTING'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 print(os.environ['APP_SETTINGS'])
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def classifier():
 
-  req = request.get_json()
-  # req = json.loads(req)
-  print(type(req))
-  df = json_to_dataframe(req)
+  #Config
+  vac = {'database': 'sm-web', 'collection': 'vacancies', 'filter': {'analized': False}, 'projection': {}}
+  
+  vac_db = MongoAPI(vac)
+  new_vacancies = vac_db.read()
+  new_vacancies_id = []
+  for i in new_vacancies:
+  	new_vacancies_id.append(str(i['_id']))
+
+  #Config ?
+  jobstr = {'database': 'sm-web', 'collection': 'jobstrings', 'filter': {'vacancyId': {'$in': new_vacancies_id}}, 'projection': {'tag': 1, 'text': 1, 'target': 1, 'vacancyId': 1}}
+  
+  jobstr_db = MongoAPI(jobstr)
+  new_jobstr = jobstr_db.read()
+
+  df = json_to_dataframe(new_jobstr)
   dataset = dataset_preparation(df)
   targets = prediction(dataset)
-  for i in range(len(req['jobs'])):
-    req['jobs'][i]['target'] = int(targets[i])
-  print(type(req))
-  return jsonify(req)
+
+
+  res = []
+
+  for i in range(len(new_jobstr)):
+    new_jobstr[i]['target'] = int(targets[i])
+    data = {'filter': {'_id': new_jobstr[i]['_id']}, 'updated_data': {'$set': {'target': new_jobstr[i]['target']}}}
+    res.append(jobstr_db.update(data))
+
+  if res.count('Nothing was updated') == 0:
+    for i in new_vacancies:
+      data = {'filter': {'_id': i['_id']}, 'updated_data': {'$set': {'analized': True}}}
+      res = vac_db.update(data)
+      print(res)
+
+  # return jsonify(req)
+
+@app.route('/analyze/<position>', methods=['GET'])
+def analyzer():
+  pass
 
 if __name__ == '__main__':
     app.run()
--- preprocessing.py
+++ preprocessing.py
@@ -10,23 +10,26 @@ def json_to_dataframe(req):
 
   """ Function takes json and returns pandas DataFrame"""
 
-  first_level_normalizing = json_normalize(req['jobs'])
-  first_level_columns = list(first_level_normalizing.columns)
+  first_level_normalizing = json_normalize(req)
+
+  works_data = first_level_normalizing
+
+  # first_level_columns = list(first_level_normalizing.columns)
   
-  works_data = json_normalize(data=req['jobs'], record_path='parsedArray', meta=first_level_columns)
-  works_data.drop(columns=['parsedArray', 'createdAt', 'updatedAt', '__v', 'keyword', 'isHeader', '_id'], inplace=True)
+  # works_data = json_normalize(data=req['jobs'], record_path='parsedArray', meta=first_level_columns)
+  # works_data.drop(columns=['parsedArray', 'createdAt', 'updatedAt', '__v', 'keyword', 'isHeader', '_id'], inplace=True)
 
-  years = works_data[works_data.text.str.contains('years', regex=False)]
-  years = years['text'].str.extract('(\d+)').dropna().rename(columns={0: 'experience'})
-  years['experience'] = years['experience'].map(int)
-  years = years.drop(years[years['experience'] > 7].index)
+  # years = works_data[works_data.text.str.contains('years', regex=False)]
+  # years = years['text'].str.extract('(\d+)').dropna().rename(columns={0: 'experience'})
+  # years['experience'] = years['experience'].map(int)
+  # years = years.drop(years[years['experience'] > 7].index)
 
-  works_data = works_data.join(years, how='outer')
-  works_data['experience'] = works_data['experience'].fillna(works_data.groupby('job_id')['experience'].transform('max')).fillna(0)
-  works_data['experience'] = works_data['experience'].map(int)
+  # works_data = works_data.join(years, how='outer')
+  # works_data['experience'] = works_data['experience'].fillna(works_data.groupby('job_id')['experience'].transform('max')).fillna(0)
+  # works_data['experience'] = works_data['experience'].map(int)
 
-  works_data['target'] = np.nan
-  works_data.drop_duplicates(subset=['text', 'job_id'], inplace=True)
+  # works_data['target'] = np.nan
+  # works_data.drop_duplicates(subset=['text', 'vacancyId'], inplace=True)
 
   print(works_data.columns)
 
