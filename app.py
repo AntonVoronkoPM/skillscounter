a1c4b18cdbe242d9b03a630f841063c5642fb1c4
Anton Voronko <avor@corp.isddesign.com>
2020-11-10 11:57:48 +0200

    analyzed field renamed

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ print(os.environ['APP_SETTINGS'])
 def classifier():
 
   #Config
-  vac = {'database': 'sm-web', 'collection': 'vacancies', 'filter': {'analized': False}, 'projection': {'analized': 1}}
+  vac = {'database': 'sm-web', 'collection': 'vacancies', 'filter': {'analyzed': False}, 'projection': {'analized': 1}}
   
   vac_db = MongoAPI(vac)
   new_vacancies = vac_db.read()
