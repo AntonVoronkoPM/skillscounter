8f88a07663308317553b66a87d2d2bda22959bab
Anton Voronko <avor@corp.isddesign.com>
2020-11-11 23:07:34 +0200

    intermediate fix

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def classifier():
   jobstr_db = MongoAPI(jobstr)
   new_jobstr = jobstr_db.read()
 
-  new_jobstr = new_jobstr[:600]
+  new_jobstr = new_jobstr[:50000]
 
   if len(new_jobstr) == 0:
     for i in new_vacancies:
